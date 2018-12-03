98de26b0093b239d1f0c353ae9397173970b336f
Christopher Hulsey (CCHULS5514) <CCHULS5514@ung.edu>
2018-12-03 18:02:16 -0500

    Gave inputs for converstion from, to, and the amount

--- currency converter.py
+++ currency converter.py
@@ -7,3 +7,10 @@ For the amount it will be a float so "" is not necessary.
 print (" Choose from these types of Currency: BGN, CAD, BRL, HUF, DKK, JPY, ILS, TRY, RON, GBP, PHP, HRK, NOK, USD, MXN, AUD, IDR, KRW, HKD, ZAR, ISK, CZK, THB, MYR, NZD, PLN, SEK, RUB, CNY, SGD, CHF, INR, EUR")
 
 """ This is all possible currencies the API supports"""
+base = input("Convert from: ")
+to = input("Convert to: ")
+amount = float(input("Amount: "))
+
+""" Ensure that when writing the base and to, to insert as a string. The Amount
+    is a float so you do not have to add "".
+    """
