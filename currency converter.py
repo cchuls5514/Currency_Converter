66b7afe6a5f22406ed4e58e98058eabb2b333615
Christopher Hulsey (CCHULS5514) <CCHULS5514@ung.edu>
2018-12-03 17:58:17 -0500

    Inserted types of currency possible for exchange

---
+++ currency converter.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+""" Make sure to type in your inputs as a string so use "" for base and to.
+For the amount it will be a float so "" is not necessary.
+"""
+print (" Choose from these types of Currency: BGN, CAD, BRL, HUF, DKK, JPY, ILS, TRY, RON, GBP, PHP, HRK, NOK, USD, MXN, AUD, IDR, KRW, HKD, ZAR, ISK, CZK, THB, MYR, NZD, PLN, SEK, RUB, CNY, SGD, CHF, INR, EUR")
+
+""" This is all possible currencies the API supports"""
