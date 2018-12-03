ded2b7231c93cc45519cfc87bab1032bd7123b5f
Christopher Hulsey (CCHULS5514) <CCHULS5514@ung.edu>
2018-12-03 18:10:32 -0500

    Added URL of API with live rates

--- currency converter.py
+++ currency converter.py
@@ -7,10 +7,11 @@ For the amount it will be a float so "" is not necessary.
 print (" Choose from these types of Currency: BGN, CAD, BRL, HUF, DKK, JPY, ILS, TRY, RON, GBP, PHP, HRK, NOK, USD, MXN, AUD, IDR, KRW, HKD, ZAR, ISK, CZK, THB, MYR, NZD, PLN, SEK, RUB, CNY, SGD, CHF, INR, EUR")
 
 """ This is all possible currencies the API supports"""
-base = input("Convert from: ")
-to = input("Convert to: ")
-amount = float(input("Amount: "))
+base = raw_input("Convert from: ")
+to = raw_input("Convert to: ")
+amount = float(raw_input("Amount: "))
 
 """ Ensure that when writing the base and to, to insert as a string. The Amount
     is a float so you do not have to add "".
     """
+url= "https://api.exchangeratesapi.io/latest?base=" + base
